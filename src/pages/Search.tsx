031f666a63b4880d6cbf1ee8430140cd8291cd48
eyergens <elizabeth.yergens@outlook.com>
2026-04-08 11:07:09 -0500

    update search page for make, model, and year

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,52 +1,90 @@
-import {Box, Paper, Typography, InputLabel, OutlinedInput, FormControl, Button} from '@mui/material'
-import React, {useState} from "react";
-import axios from "axios";
+import {
+  Box,
+  Paper,
+  Typography,
+  Button,
+  Autocomplete,
+  TextField
+} from '@mui/material'
+import React, {useEffect, useState} from "react";
+import axios, {type AxiosError} from "axios";
 import {useQuery} from "@tanstack/react-query";
 import SearchIcon from '@mui/icons-material/Search';
-import GroupedModels from "../components/GroupedModels.tsx";
+import carMakesCSV from '../lib/makes.csv?raw';
+import Papa from 'papaparse';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs';
+import {DatePicker} from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
-interface SearchResult {
+const apiKey = import.meta.env.VITE_CAR_API_KEY;
+
+interface ModelSearchResult {
   Count: number;
   Message: string;
   SearchCriteria: string;
   Results: [];
 }
 
-export interface ModelProps {
+interface ValueSearchResult {
+  make: string;
+  model: string;
+  year: number;
+  valuationPrice: number;
+  currency: string;
+  country: string;
+}
+
+export interface Model {
   Make_ID: number;
   Make_Name: string;
   Model_ID: number;
   Model_Name: string;
 }
 
-function groupByMake(cars: ModelProps[]): Record<string, ModelProps[]> {
-  return cars.reduce((grouped, car) => {
-    if (!grouped[car.Make_Name]) {
-      grouped[car.Make_Name] = [];
-    }
-    grouped[car.Make_Name].push(car);
-    return grouped;
-  }, {} as Record<string, ModelProps[]>);
-}
+const fetchModelSearchResults = async (query: string): Promise<ModelSearchResult> => {
+  const {data} = await axios.get<ModelSearchResult>(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${query}?format=json`);
+  return data;
+};
 
-const fetchSearchResults = async (query: string): Promise<SearchResult> => {
-  const {data} = await axios.get<SearchResult>(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformake/${query}?format=json`);
+const fetchMarketValueSearchResults = async (make: string, model: string, year: number): Promise<ValueSearchResult> => {
+  const {data} = await axios.get<ValueSearchResult>(`https://api.carapi.dev/v1/vehicle-valuation?token=${apiKey}&make=${make}&model=${model}&year=${year}&country=US`);
   return data;
 };
 
 export default function Search() {
-  const [searchTerm, setSearchTerm] = useState('');
-  const [activeQuery, setActiveQuery] = useState('');
+  const [carMakes, setCarMakes] = useState<string[]>([]);
+  const [make, setMake] = useState('');
+  const [model, setModel] = useState('');
+  const [year, setYear] = useState(dayjs());
+  const [filters, setFilters] = useState({make: '', model: '', year: dayjs().year()});
 
-  const {data, isLoading, isError, error} = useQuery({
-    queryKey: ['search', activeQuery],
-    queryFn: () => fetchSearchResults(activeQuery),
-    enabled: !!activeQuery,
+  useEffect(() => {
+    Papa.parse(carMakesCSV, {
+      header: false,
+      skipEmptyLines: true,
+      complete: (results: { data: string[][]; }) => {
+        const makes = results.data.map((row: string[]) => row[0]);
+        setCarMakes(makes);
+      },
+    });
+  }, []);
+
+  const modelsQuery = useQuery({
+    queryKey: ['search', make],
+    queryFn: () => fetchModelSearchResults(make),
+    enabled: !!make,
+  })
+
+  const valueQuery = useQuery({
+    queryKey: ['search', filters],
+    queryFn: () => fetchMarketValueSearchResults(make, model, year.year()),
+    enabled: !!filters.make && !!filters.model && !!filters.year,
   })
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    setActiveQuery(searchTerm);
+    setFilters({make: make, model: model, year: year.year()});
   };
 
   return (
@@ -57,17 +95,63 @@ export default function Search() {
           sx={{padding: '16px', marginBottom: '16px'}}
           elevation={3}
         >
-          <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
-            <InputLabel htmlFor='make'>Make</InputLabel>
-            <OutlinedInput
-              required
-              label="Make"
-              type="text"
-              name="make"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-          </FormControl>
+          <Autocomplete
+            options={carMakes}
+            getOptionLabel={(option) => option}
+            value={make}
+            onChange={(_event, newValue) => {
+              setModel('');
+              setMake(newValue ?? '');
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Make"
+                variant="outlined"
+                required
+                sx={{m: 1, width: '25ch'}}
+              />
+            )}
+          />
+          <Autocomplete
+            options={modelsQuery.data?.Results.map((model: Model) => (model.Model_Name)) || []}
+            getOptionLabel={option => option}
+            noOptionsText={
+              modelsQuery.isError
+                ? "Error loading models"
+                : (modelsQuery.isLoading ? "Loading options..." : "Select a make first")
+            }
+            value={model}
+            onChange={(_event, newValue) => {
+              setModel(newValue ?? '')
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Model"
+                variant="outlined"
+                required
+                sx={{m: 1, width: '25ch'}}
+              />
+            )}
+          />
+          <div>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DatePicker
+                label="Year"
+                views={['year']}
+                sx={{m: 1, width: '25ch'}}
+                maxDate={dayjs()}
+                value={year}
+                onChange={(newValue) => setYear(newValue ?? dayjs())}
+                slotProps={{
+                  textField: {
+                    required: true,
+                  },
+                }}
+              />
+            </LocalizationProvider>
+          </div>
           <Button
             sx={{
               padding: '16px',
@@ -84,10 +168,14 @@ export default function Search() {
           </Button>
         </Paper>
 
-        {isLoading && <p>Loading...</p>}
-        {isError && <p>Error: {(error as Error).message}</p>}
-
-        { data != undefined ? <GroupedModels groupedModels={groupByMake(data?.Results)}/> : ''}
+        {valueQuery.isLoading && <p>Loading...</p>}
+        {valueQuery.isError &&
+          <Typography variant={"h4"}>
+            Error: {((valueQuery.error as AxiosError).response?.data as { error: string }).error}
+          </Typography>
+        }
+        {valueQuery.data != undefined ?
+          <Typography variant={"h4"}>Estimated Value: ${valueQuery.data.valuationPrice}</Typography> : ''}
       </Box>
     </>
   );
