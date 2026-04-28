45ce5fcf9266193d779f0a2115ae41ec04bc31ad
eyergens <elizabeth.yergens@outlook.com>
2026-04-28 14:47:50 -0500

    remove unneeded fragments

--- app/src/components/Form.tsx
+++ app/src/components/Form.tsx
@@ -38,90 +38,88 @@ export default function Form({addQuoteOption}: QuoteFormProps) {
   };
 
   return (
-    <>
-      <Box p={2} component="form" onSubmit={handleSubmit}>
-        <Paper
-          sx={{padding: '16px', marginBottom: '16px'}}
-          elevation={3}
+    <Box p={2} component="form" onSubmit={handleSubmit}>
+      <Paper
+        sx={{padding: '16px', marginBottom: '16px'}}
+        elevation={3}
+      >
+        <Typography variant="h6">New Quote Option</Typography>
+        <Box display="flex" flexDirection="column" gap={2} mt={2}>
+          <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
+            <InputLabel htmlFor='downPayment'>Down Payment</InputLabel>
+            <OutlinedInput
+              required
+              label="Down Payment"
+              type="number"
+              name="downPayment"
+              value={newQuote.downPayment}
+              onChange={handleChange}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              inputProps={{
+                min: 0
+              }}
+            />
+          </FormControl>
+          <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
+            <InputLabel htmlFor='monthlyRate'>Monthly Rate</InputLabel>
+            <OutlinedInput
+              required
+              label="Monthly Rate"
+              type="number"
+              name="monthlyRate"
+              value={newQuote.monthlyRate}
+              onChange={handleChange}
+              startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              inputProps={{
+                min: 0
+              }}
+            />
+          </FormControl>
+          <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
+            <InputLabel htmlFor='term'>Term (months)</InputLabel>
+            <OutlinedInput
+              required
+              label="Term (months)"
+              type="number"
+              name="term"
+              value={newQuote.term}
+              onChange={handleChange}
+              inputProps={{
+                min: 0
+              }}
+            />
+          </FormControl>
+          <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
+            <InputLabel htmlFor='interestRate'>Interest Rate</InputLabel>
+            <OutlinedInput
+              required
+              label="Interest Rate"
+              type="number"
+              name="interestRate"
+              value={newQuote.interestRate}
+              onChange={handleChange}
+              endAdornment={<InputAdornment position="start">%</InputAdornment>}
+              inputProps={{
+                step: "0.01",
+                min: 0
+              }}
+            />
+          </FormControl>
+        </Box>
+        <Button
+          sx={{
+            py: 1.5,
+            fontWeight: 'bold',
+            marginTop: '0.5rem'
+          }}
+          type="submit"
+          variant="outlined"
+          startIcon={<AddIcon/>}
+          onClick={handleSubmit}
         >
-          <Typography variant="h6">New Quote Option</Typography>
-          <Box display="flex" flexDirection="column" gap={2} mt={2}>
-            <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
-              <InputLabel htmlFor='downPayment'>Down Payment</InputLabel>
-              <OutlinedInput
-                required
-                label="Down Payment"
-                type="number"
-                name="downPayment"
-                value={newQuote.downPayment}
-                onChange={handleChange}
-                startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                inputProps={{
-                  min: 0
-                }}
-              />
-            </FormControl>
-            <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
-              <InputLabel htmlFor='monthlyRate'>Monthly Rate</InputLabel>
-              <OutlinedInput
-                required
-                label="Monthly Rate"
-                type="number"
-                name="monthlyRate"
-                value={newQuote.monthlyRate}
-                onChange={handleChange}
-                startAdornment={<InputAdornment position="start">$</InputAdornment>}
-                inputProps={{
-                  min: 0
-                }}
-              />
-            </FormControl>
-            <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
-              <InputLabel htmlFor='term'>Term (months)</InputLabel>
-              <OutlinedInput
-                required
-                label="Term (months)"
-                type="number"
-                name="term"
-                value={newQuote.term}
-                onChange={handleChange}
-                inputProps={{
-                  min: 0
-                }}
-              />
-            </FormControl>
-            <FormControl sx={{m: 1, width: '25ch', alignSelf: 'center'}} variant="outlined">
-              <InputLabel htmlFor='interestRate'>Interest Rate</InputLabel>
-              <OutlinedInput
-                required
-                label="Interest Rate"
-                type="number"
-                name="interestRate"
-                value={newQuote.interestRate}
-                onChange={handleChange}
-                endAdornment={<InputAdornment position="start">%</InputAdornment>}
-                inputProps={{
-                  step: "0.01",
-                  min: 0
-                }}
-              />
-            </FormControl>
-          </Box>
-          <Button
-            sx={{
-              py: 1.5,
-              fontWeight: 'bold',
-              marginTop: '0.5rem'
-            }}
-            type="submit"
-            variant="outlined"
-            startIcon={<AddIcon/>}
-            onClick={handleSubmit}
-          >
-            Add Quote
-          </Button>
-        </Paper>
-      </Box>
-    </>
+          Add Quote
+        </Button>
+      </Paper>
+    </Box>
   );
 }
--- app/src/components/Option.tsx
+++ app/src/components/Option.tsx
@@ -9,27 +9,28 @@ interface OptionProps {
   interestRate: number,
 }
 
-export default function Option({option, removeQuoteOption}: {option: OptionProps, removeQuoteOption: (id: number) => void}) {
+export default function Option({option, removeQuoteOption}: {
+  option: OptionProps,
+  removeQuoteOption: (id: number) => void
+}) {
   return (
-    <>
-      <Paper
-        sx={{padding: '16px', margin: '16px'}}
-        key={option.id}
-        elevation={3}
-      >
-        <Box display="flex" flexDirection="row" alignItems="center">
-          <Typography variant="h5">Quote Option {option.id}</Typography>
-          <IconButton onClick={() => removeQuoteOption(option.id)}>
-            <DeleteIcon></DeleteIcon>
-          </IconButton>
-        </Box>
-        <Box display="flex" flexDirection="column" gap={2} mt={2}>
-          <Typography>Down Payment: ${option.downPayment}</Typography>
-          <Typography>Monthly Rate: ${option.monthlyRate}</Typography>
-          <Typography>Term (months): {option.term}</Typography>
-          <Typography>Interest Rate: {option.interestRate}%</Typography>
-        </Box>
-      </Paper>
-    </>
+    <Paper
+      sx={{padding: '16px', margin: '16px'}}
+      key={option.id}
+      elevation={3}
+    >
+      <Box display="flex" flexDirection="row" alignItems="center">
+        <Typography variant="h5">Quote Option {option.id}</Typography>
+        <IconButton onClick={() => removeQuoteOption(option.id)}>
+          <DeleteIcon></DeleteIcon>
+        </IconButton>
+      </Box>
+      <Box display="flex" flexDirection="column" gap={2} mt={2}>
+        <Typography>Down Payment: ${option.downPayment}</Typography>
+        <Typography>Monthly Rate: ${option.monthlyRate}</Typography>
+        <Typography>Term (months): {option.term}</Typography>
+        <Typography>Interest Rate: {option.interestRate}%</Typography>
+      </Box>
+    </Paper>
   );
 }
